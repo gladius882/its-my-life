41417dcc8b7febfe1b666ea18838df46800ed156
Patryk Spychalski <p.spychalski@satfilm.pl>
2025-06-05 12:42:55 +0200

    session provider in main layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,19 +1,15 @@
 import { ReactNode } from "react";
 import { getServerSession } from "next-auth";
 import { authOptions } from "@/app/api/auth/[...nextauth]/route";
+import { Providers } from "./providers";
 
 export default async function RootLayout({ children }: { children: ReactNode }) {
-	const session = await getServerSession(authOptions);
-
-	if(!session) {
-		return (
-			<h1>Unauthorized</h1>
-		)
-	}
 
 	return (
 		<html lang="en">
-			<body>{children}</body>
+			<body>
+				<Providers>{children}</Providers>
+			</body>
 		</html>
 	);
 }
