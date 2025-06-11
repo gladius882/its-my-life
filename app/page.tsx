da442e60a21cee9c9f8ec2ee14d4c8f0865fef21
gladius882 <22199518+gladius882@users.noreply.github.com>
2025-06-11 19:03:21 +0200

    basic topbar style

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,11 @@
 "use client"
 
-import { signOut, useSession } from "next-auth/react";
-import Link from "next/link";
 
 export default function Home() {
-  const {data: session, status} = useSession();
 
   return (
     <>
-      <p>
-        Status: {status}
-      </p>
 
-      <Link href={'/login'}>Zaloguj</Link><br />
-      <Link href={'/register'}>Utwórz konto</Link><br />
-      <button onClick={() => signOut()}>Wyloguj</button>
     </>
   );
 }
--- components/TopBar.tsx
+++ components/TopBar.tsx
@@ -1,6 +1,29 @@
+"use client"
+
+import { signOut, useSession } from "next-auth/react";
+
 const TopBar = () => {
+    const { data: session, status } = useSession();
+
     return (
-        <div>Topbar</div>
+        <div className="flex justify-between h-13 p-5">
+
+            <div className="flex flex-col justify-center">
+                Status: {status}<br/>
+                {session?.user?.name}
+            </div>
+
+            <div className="w-50 flex flex-col justify-center w-100">
+                <input type="text" placeholder="Szukaj" className="border w-full p-2 rounded-lg border-gray-500"/>
+            </div>
+
+            <div className="flex flex-col justify-center">
+                <button onClick={() => signOut()} className="border p-2 hover:bg-[#4880FF]">
+                    Wyloguj
+                </button>
+            </div>
+
+        </div>
     )
 }
 
