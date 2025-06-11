7412857dc65c96d7b9bc54ac39f06fe200877d6d
Patryk Spychalski <p.spychalski@satfilm.pl>
2025-06-11 08:31:49 +0200

    full width layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Providers } from "./providers";
 import { loadPlugins } from "@/core/pluginManager";
 import "./globals.css"
 import Navigation from "@/components/Navigation";
+import TopBar from "@/components/TopBar";
 
 export default async function RootLayout({ children }: { children: ReactNode }) {
 
@@ -11,18 +12,20 @@ export default async function RootLayout({ children }: { children: ReactNode }) 
 	return (
 		<html lang="en">
 			<Providers>
-			<body className="flex flex-grow">
+				<body>
 
-				<Navigation />
+					<div id="container" className="flex flex-grow">
+						<Navigation />
 
-				<div>
-					<div>Topbar</div>
+						<div className="w-full">
+							<TopBar />
 
-					<main className="bg-[#F5F6FA]">
-						{children}
-					</main>
-				</div>
-			</body>
+							<main className="bg-[#F5F6FA]">
+								{children}
+							</main>
+						</div>
+					</div>
+				</body>
 			</Providers>
 		</html>
 	);
