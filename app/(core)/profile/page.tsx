f942c316fbc765a379a87903b71548a34f058554
Patryk Spychalski <p.spychalski@satfilm.pl>
2025-06-13 09:59:57 +0200

    simple profile page

--- app/(core)/profile/page.tsx
+++ app/(core)/profile/page.tsx
@@ -1,6 +1,26 @@
+"use client"
+
+import { useSession } from "next-auth/react";
+
 const ProfilePage = () => {
+    const { data: session } = useSession();
+
     return (
-        <p>Profile</p>
+        <div className="flex justify-center">
+            <div className="w-2/3 flex flex-col gap-4">
+
+                <div>
+                    <label>Login</label>
+                    <input type="text" value={session?.user?.name || ""} className="border w-full p-2 rounded-lg border-gray-500" />
+                </div>
+
+                <div>
+                    <label>Login</label>
+                    <input type="text" value={session?.user?.name || ""} className="border w-full p-2 rounded-lg border-gray-500" />
+                </div>
+
+            </div>
+        </div>
     )
 }
 
